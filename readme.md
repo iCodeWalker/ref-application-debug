883abdb52bca62db0f7d403d74a28f2f3d0c587b
iCodeWalker <vaibhav3kadwey@gmail.com>
2024-05-27 23:21:39 +0530

    added : debug ui leva

--- readme.md
+++ readme.md
@@ -18,3 +18,19 @@
     React Developer tools extention will help us check and modify react components live.
 
 ## Debug UI with Leva
+
+    npm i leva
+
+    Tweaking a value, will result in the component re-rendering.
+    const controls = useControls({ position: -2})
+
+    We can destructure it in order to get only the position.
+    const { position } = useControls({ position : -2 })
+
+    Now we can use this position variable in the position-x attribute of the sphere <mesh>
+
+    Instead of adding a second range tweak, we can use a vector 2 tweak.
+
+    value : {x : -2, y: 0}
+
+    We can use colors. Add acolor property in the useControls and a color variable in the destructuring.
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,24 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
 
 export default function Experience()
 {
+
+    // const controls = useControls({ position: -2})
+    // const { position } = useControls({ position : -2 })
+
+    const controls = useControls({
+    position: {
+        // value: -2,
+        value : {x : -2, y: 0, z:0},
+        min: -4,
+        max:4,
+        step: 0.01,
+        joystick:"invertY" // To invert the y-direction
+    },
+    color : '#ff0000'
+})
+
     return <>
 
         <OrbitControls makeDefault />
@@ -9,9 +26,10 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
+        {/* <mesh position-x={controls.position}> */}
+        <mesh position={[controls.position.x, controls.position.y, controls.position.z]}>
             <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+            <meshStandardMaterial color={controls.color} />
         </mesh>
 
         <mesh position-x={ 2 } scale={ 1.5 }>
